71ec86ff8832ec631bc7377dea910783065f907e
Maycon-crz <mayconnascimentodeoliveira23@gmail.com>
2023-08-02 23:43:06 -0300

    Certificado e códigos

---
+++ codigos/00_primeira_funcao.py
@@ -0,0 +1,13 @@
+def exibir_mensagem_1():
+    print("Olá mundo!")
+    
+def exibir_mensagem_2(nome):
+    print(f"Seja bem vindo {nome}")
+    
+def exibir_mensagem_3(nome="Maycon"):
+    print(f"Seja bem vindo {nome} *")
+    
+# exibir_mensagem_1()
+# exibir_mensagem_2(nome="Maycon")
+exibir_mensagem_3()
+# exibir_mensagem_3(nome="Maycon")
---
+++ codigos/03_dicionarios_aninhados.py
@@ -0,0 +1,6 @@
+contatos = {
+    "email_1@gmail.com": {"nome1", "", "telefone", ""},
+    "email_2@gmail.com": {"nome1", "", "telefone", ""},
+    "email_3@gmail.com": {"nome1", "", "telefone", ""},
+    "email_4@gmail.com": {"nome1", "", "telefone", ""},
+}
